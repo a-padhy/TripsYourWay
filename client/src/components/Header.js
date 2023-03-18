1f7df8f5d594a0632d0b3f28e96a3b778f9bc543
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2023-03-19 04:16:01 +0530

    ui fix

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,15 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../UserContext";
+import { MdOutlineTravelExplore } from "react-icons/md";
 
 const Header = () => {
   const { user } = useContext(UserContext);
   return (
     <header className="flex justify-between items-center">
       <Link to={"/"} className="flex gap-1 items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-8 h-8 -rotate-90 "
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5"
-          />
-        </svg>
-        <span className="font-bold text-xl">airbnc</span>
+        <MdOutlineTravelExplore className="w-8 h-8" />
+        <span className="font-bold text-xl">TripsYourWay</span>
       </Link>
       <div className="flex gap-2 border border-gray-300 rounded-full px-4 py-2 shadow-md shadow-gray-300">
         <div>Anywhere</div>
