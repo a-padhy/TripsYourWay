5ef01c0c94fde6bf35ba11bd52d83d5154b2babd
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2023-05-02 03:14:14 +0530

    final 1.x.1

--- server/index.js
+++ server/index.js
@@ -54,46 +54,6 @@ app.get("/test", (req, res) => {
   res.json("test ok");
 });
 
-// app.post("/register", async (req, res) => {
-//   const { name, email, password } = req.body;
-//   try {
-//     const userDoc = await User.create({
-//       name,
-//       email,
-//       password: bcrypt.hashSync(password, bcryptSalt),
-//     });
-//     res.json(userDoc);
-//   } catch (e) {
-//     res.status(422).json(e);
-//   }
-// });
-
-// app.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
-//   const userDoc = await User.findOne({ email });
-//   if (userDoc) {
-//     const passOk = bcrypt.compareSync(password, userDoc.password);
-//     if (passOk) {
-//       jwt.sign(
-//         {
-//           email: userDoc.email,
-//           id: userDoc._id,
-//         },
-//         jwtSecret,
-//         {},
-//         (err, token) => {
-//           if (err) throw err;
-//           res.cookie("token", token).json(userDoc);
-//         }
-//       );
-//     } else {
-//       res.status(422).json("pass not ok");
-//     }
-//   } else {
-//     res.json("not found");
-//   }
-// });
-
 app.get("/profile", (req, res) => {
   const { token } = req.cookies;
   if (token) {
@@ -106,10 +66,6 @@ app.get("/profile", (req, res) => {
     res.json(null);
   }
 });
-
-// app.post("/logout", (req, res) => {
-//   res.cookie("token", "").json(true);
-// });
 
 app.post("/upload-by-link", async (req, res) => {
   const { link } = req.body;
